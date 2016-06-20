b0faa579b44f2e6f58a73e942cecf035c2103411
Dominic Tarr <dominic.tarr@gmail.com>
2016-06-21 06:29:31 +1200

    depceciation warning for tail option

--- read.js
+++ read.js
@@ -4,6 +4,10 @@ var old = require('./old')
 var live = require('./live')
 
 module.exports = function (db, opts) {
+  if(opts && opts.tail) {
+    console.error('pull-level: .tail option is depreciated. use .live instead')
+    opts.live = opts.tail
+  }
   return Live(function (opts) {
     return old(db, opts)
   }, function (opts) {
